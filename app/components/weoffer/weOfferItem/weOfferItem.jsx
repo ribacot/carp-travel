ab132a422e664997edee1418cb7286df187edc4e
ribacot <remontdiskov1979@gmail.com>
2023-10-09 00:01:22 +0300

    next step)

--- app/components/weoffer/weOfferItem/weOfferItem.jsx
+++ app/components/weoffer/weOfferItem/weOfferItem.jsx
@@ -2,24 +2,25 @@ import Image from "next/image";
 import css from "./weOfferItem.module.css";
 import ListOffers from "../listOffers/listOffers";
 export default function WeOfferItem({ item, chengeCard, numItem }) {
-  const { img, tagline, description, title } = item;
-  return (
-    <div className={css.wrapperItem}>
-      <Image className={css.img} src={img} alt={title} />
+	const { img, tagline, description, title } = item;
+	return (
+		<div className={css.wrapperItem}>
+			<Image className={css.img} src={img} alt={title} />
 
-      <p className={css.titleExempleMobile}>{tagline}</p>
-      <div className={css.decor}>
+			<div className={css.decor}>
+				<p className={css.titleExemple}>{tagline}</p>
         <ListOffers
-          style={css.activTablet}
-          onClick={chengeCard}
-          numItem={numItem}
-          tagline={tagline}
-        />
+						style={css.activTablet}
+						onClick={chengeCard}
+						numItem={numItem}
+						tagline={tagline}
+					/>
 
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+				{/* <div className={css.decor}> */}
+
+					<p className={css.descriptionOffer}>{description}</p>
+				{/* </div> */}
+			</div>
+		</div>
+	);
 }
-
-// width={280} height={213}
