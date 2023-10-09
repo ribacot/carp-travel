f855369c87d1651ea8e09f7ac26a8fd0104faa1a
ribacot <remontdiskov1979@gmail.com>
2023-10-10 01:29:11 +0300

    weOffer_end

--- app/components/weoffer/listOffers/listOffers.jsx
+++ app/components/weoffer/listOffers/listOffers.jsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
-import {exemplesArr} from "../examples";
+import { exemplesArr } from "../examples";
 import css from "./listOffers.module.css";
-import activeImg from "@/app/img/weoffer/button_active.svg"
+import activeImg from "@/app/img/weoffer/button_active.svg";
 
-export default function ListOffers({ onClick, numItem,tagline,style }) {
-  console.log(style)
-  return (<div>    <ul className={`${css.listoffers} ${style}`}>
-    {exemplesArr.map(({ title }, i) => (
-      <li key={title}>
-        <button type="button" onClick={onClick} id={i} className={`${css.btnChenge} ${numItem === i + 1 && css.btnActive}`}>
-          {numItem === i + 1 && <Image src={activeImg} alt="active button"/>}{title}
-        </button>
-      </li>
-    ))}
-  </ul>
-  <p className={css.titleExempleTablet}>{tagline}</p>
-
-</div>
-  );
+export default function ListOffers({ onClick, numItem, style }) {
+	console.log(style);
+	return (
+		<div className={style}>
+			<ul className={css.listoffers}>
+				{exemplesArr.map(({ title }, i) => (
+					<li key={title}>
+						<button
+							type="button"
+							onClick={onClick}
+							id={i}
+							className={`${css.btnChenge} ${numItem === i + 1 && css.btnActive}`}
+						>
+							{numItem === i + 1 && <Image src={activeImg} alt="active button" />}
+							{title}
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
--- app/components/weoffer/weOfferItem/weOfferItem.jsx
+++ app/components/weoffer/weOfferItem/weOfferItem.jsx
@@ -6,20 +6,24 @@ export default function WeOfferItem({ item, chengeCard, numItem }) {
 	return (
 		<div className={css.wrapperItem}>
 			<Image className={css.img} src={img} alt={title} />
-
-			<div className={css.decor}>
-				<p className={css.titleExemple}>{tagline}</p>
-        <ListOffers
-						style={css.activTablet}
+      <ListOffers
+						style={css.activeDesctop}
 						onClick={chengeCard}
 						numItem={numItem}
 						tagline={tagline}
 					/>
 
-				{/* <div className={css.decor}> */}
+			<div className={css.decor}>
+				<p className={css.titleExemple}>{tagline}</p>
+        <ListOffers
+						style={css.activeTablet}
+						onClick={chengeCard}
+						numItem={numItem}
+						tagline={tagline}
+					/>
+
 
 					<p className={css.descriptionOffer}>{description}</p>
-				{/* </div> */}
 			</div>
 		</div>
 	);
