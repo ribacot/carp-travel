aaea75eeae9e6fc92092855e452aef7dae7776f4
ribacot <remontdiskov1979@gmail.com>
2023-11-15 01:42:22 +0200

    slader

--- app/components/btnNavGallery/btnNavGallery.jsx
+++ app/components/btnNavGallery/btnNavGallery.jsx
@@ -1,23 +1,13 @@
 "use client"
 import css from "./btnNavGallery.module.css"
 
-// import img1 from "../../img/gallery/Mask group.png";
-// import img2 from "../../img/gallery/Mask group-1.png";
-// import img3 from "../../img/gallery/Mask group-2.png";
-// const imgArr = [img1, img2, img3];
 
 
-export default function BtnNavGallery({ children,  style }) {
-	// const onChengeImg = () => {
-	// const arr=	imgArr.map((el, idx, arr) => {
-	// 		idx + 1 > arr.length ? el[0] : el[idx + 1];
-	// 	});
-	// 	console.log(arr);
-	// };
+export default function BtnNavGallery({ children,  style ,onChengeImg}) {
 
 
 	return (
-		<button type="button" className={`${css.btn} ${style}`}>
+		<button type="button" className={`${css.btn} ${style}`} onClick={onChengeImg}>
 			{children}
 		</button>
 	);
--- app/components/gallery/gallery.jsx
+++ app/components/gallery/gallery.jsx
@@ -1,4 +1,4 @@
-// "use client"
+"use client";
 
 import Image from "next/image";
 
@@ -6,40 +6,67 @@ import Background from "../background/backgroun";
 import Section from "../section/section";
 
 import imgBg from "../../img/section_bacground/gallery.png";
-import img1 from "../../img/gallery/Mask group.png";
-import img2 from "../../img/gallery/Mask group-1.png";
-import img3 from "../../img/gallery/Mask group-2.png";
+
+import img1 from "@/app/img/gallery/img1.png";
+// import img1 from "../../img/gallery/Mask group.png";
+import img2 from "@/app/img/gallery/img2.png";
+import img3 from "@/app/img/gallery/img3.png";
 
 import css from "./gallery.module.css";
 import BtnNavGallery from "../btnNavGallery/btnNavGallery";
+import { useState } from "react";
 
-const imgArr = [img1, img2, img3];
+const defaultimgArr = [img1, img2, img3];
 
 export default function Gallery() {
-	const onChengeImg = () => {
-		// imgArr.map((el, idx, arr) => {
-		// 	idx + 1 > arr.length ? el[0] : el[idx + 1];
-		// });
-		console.log("chengeImg");
-	};
+	const [imgArr, setImgArr] = useState(defaultimgArr);
+	const onChengeImgNext = () => {
+		const newArr = imgArr?.reduce((acc, el, idx, arr) => {
+			if (idx === imgArr.length - 1) {
+				acc = [el, ...arr];
+			}
+			return acc;
+		}, []);
+		newArr.splice(-1, 1);
 
+		setImgArr(newArr);
+	};
+	const onChengeImgPrev = () => {
+		const newArr = imgArr?.reduce((acc, el, idx, arr) => {
+			console.log("arr:", acc);
+			if (idx === 0) {
+				acc = [...arr, el];
+			}
+			return acc;
+		}, []);
+		newArr.splice(0, 1);
+
+		setImgArr(newArr);
+	};
 	return (
 		<Section id="gallery" title="our" span="gallery">
 			<Background url={imgBg} />
 			<div className={css.wrapperGallery}>
 				<ul className={css.imgWrapper}>
-					{imgArr.map((img, idx) => (
-						<li
-							key={img} className={`${css.imgThumb} ${idx === 1 ? css.mainImg : css.secondaryImg} ${
-								idx > 2 && css.hidden
-							}`}
-						>
-							<Image src={img} sizes="100%" alt="Тature" />
-						</li>
-					))}
+					{imgArr.map((img, idx) => {
+						return (
+							<li
+								key={img.src}
+								className={`${css.imgThumb} ${
+									idx === 1 ? css.mainImg : css.secondaryImg
+								} ${idx > 2 && css.hidden}`}
+							>
+								<Image src={img} sizes="100%" alt="Nature" />
+							</li>
+						);
+					})}
 				</ul>
-				<BtnNavGallery style={css.btnBack} >back</BtnNavGallery>
-				<BtnNavGallery style={css.btnNext} >next</BtnNavGallery>
+				<BtnNavGallery style={css.btnBack} onChengeImg={onChengeImgPrev}>
+					back
+				</BtnNavGallery>
+				<BtnNavGallery style={css.btnNext} onChengeImg={onChengeImgNext}>
+					next
+				</BtnNavGallery>
 			</div>
 		</Section>
 	);
